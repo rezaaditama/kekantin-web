001fa32defbd0b86b458dd81dd645510da3756fd
rezaaditama <aditamareza03@gmail.com>
2026-07-09 22:53:16 +0700

    v1

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 // public API (Local Development)
-// const api = axios.create({
-//   baseURL: 'http://localhost:3000/api',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// public API (Production Vercel)
 const api = axios.create({
-  baseURL: 'https://be-mobile-ecanteen.vercel.app/api',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+// public API (Production Vercel)
+// const api = axios.create({
+//   baseURL: 'https://be-mobile-ecanteen.vercel.app/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
 export default api;
